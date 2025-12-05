8ada8b0ca1d79fea8837755fcdecc48357a67a0e
David Qian <rqa4@sfu.ca>
2025-12-04 21:17:09 -0800

    update

--- convert.py
+++ convert.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pyvista as pv
 from skimage import measure
-import h5py
+from skimage.morphology import skeletonize, medial_axis
 
 
-npz_path = "voxel_and_sdf.npz"
+
+npz_path = "./reference_models_processed/dog/voxel_and_sdf.npz"
 data = np.load(npz_path)
 voxels = data['voxels']
 print(voxels.shape)
@@ -17,40 +18,31 @@ print(centroid)
 scale = data['scale']
 print(scale)
 
-
 verts, faces, normals, values = measure.marching_cubes(voxels.astype(float), level=0.5)
 faces = np.hstack([np.full((faces.shape[0], 1), 3), faces]).astype(np.int64)
 mesh_smooth = pv.PolyData(verts, faces)
 
-mesh_smooth.save("mesh_smooth.obj")
-print("Smooth mesh saved as mesh_smooth.obj")
+plotter1 = pv.Plotter(title="1")
+plotter1.add_mesh(mesh_smooth, color="white", opacity=0.5)
+plotter1.show()
 
+# # Voxel Mesh
+# x, y, z = np.where(voxels)
+# cubes = [pv.Cube(center=(xi, yi, zi), x_length=1, y_length=1, z_length=1) for xi, yi, zi in zip(x, y, z)]
+# mesh_voxel = cubes[0].merge(cubes[1:]) if cubes else None
 
-x, y, z = np.where(voxels)
-plotter = pv.Plotter(off_screen=True)
-cubes = []
+# plotter2 = pv.Plotter(title="2")
+# if mesh_voxel:
+#     plotter2.add_mesh(mesh_voxel, color="lightgray", opacity=0.3)
+# plotter2.show()
 
-for xi, yi, zi in zip(x, y, z):
-    cube = pv.Cube(center=(xi, yi, zi), x_length=1, y_length=1, z_length=1)
-    cubes.append(cube)
+# Skeleton using skeletonize
+skeleton_sk = skeletonize(voxels > 0)
+sx, sy, sz = np.where(skeleton_sk)
+skeleton_sk_points = np.vstack([sx, sy, sz]).T
 
-
-if cubes:
-    mesh_voxel = cubes[0].merge(cubes[1:])
-    mesh_voxel.save("mesh_voxel.obj")
-    print("Voxel cube mesh saved as mesh_voxel.obj")
-else:
-    print("No voxels to save as cubes.")
-
-# points = sdf_points / scale + centroid
-points = sdf_points.copy()
-
-point_cloud = pv.PolyData(points)
-point_cloud["SDF"] = sdf_values
-
-plotter = pv.Plotter()
-plotter.add_points(point_cloud, scalars="SDF", render_points_as_spheres=True, point_size=3)
-
-# plotter.add_mesh(pv.Sphere(center=centroid, radius=1.0), color="red")
-
-plotter.show()
+plotter3 = pv.Plotter(title="3")
+if skeleton_sk_points.size > 0:
+    skel_cloud_sk = pv.PolyData(skeleton_sk_points)
+    plotter3.add_points(skel_cloud_sk, color="blue", point_size=5, render_points_as_spheres=True)
+plotter3.show()
